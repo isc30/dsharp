f4272880d3184672fb5a718fb354ced1842320a0
Rosario Crisci <novagelida@gmail.com>
2019-07-10 17:18:53 +0200

    working on generic dictionary. Adding comments to arraylist

--- src/DSharp.Mscorlib/Collections/Generic/Dictionary.cs
+++ src/DSharp.Mscorlib/Collections/Generic/Dictionary.cs
@@ -8,7 +8,9 @@ namespace System.Collections.Generic
     [ScriptIgnoreNamespace]
     [ScriptImport]
     [ScriptName("Object")]
-    public sealed class Dictionary<TKey, TValue> : IEnumerable<KeyValuePair<TKey, TValue>>
+    // In CLR this is an interface (IDictionary) therefore no ctors are defined.
+    public sealed partial class Dictionary<TKey, TValue> : IEnumerable<KeyValuePair<TKey, TValue>> // In CLR is inheriting from ICollection<KeyValuePair<TKey, TValue>> where TKey : notnull.
+
     {
         public Dictionary() { }
 
@@ -17,9 +19,9 @@ namespace System.Collections.Generic
         [Obsolete("This is only for use by the c# compiler, and cannot be used for generating script.", /* error */ true)]
         public extern Dictionary(int count);
 
-        public extern int Count { get; }
+        public extern IReadOnlyCollection<TKey> Keys { get; } // Return type should be ICollection<TKey>
 
-        public extern IReadOnlyCollection<TKey> Keys { get; }
+        // We are missing the Values property -> ICollection<TValue> Values { get; }
 
         [ScriptField]
         public extern TValue this[TKey key] { get; set; }
@@ -33,16 +35,8 @@ namespace System.Collections.Generic
         [DSharpScriptMemberName("keyExists")]
         public extern bool ContainsKey(TKey key);
 
-        public extern static Dictionary<TKey, TValue> GetDictionary(object o);
-
-        public extern IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator();
-
-        extern IEnumerator IEnumerable.GetEnumerator();
-
+        // Modified to return bool. Check implementation.
         public extern void Remove(TKey key);
-
-        [Obsolete("This is only for use by the c# compiler, and cannot be used for generating script.", /* error */ true)]
-        public extern bool TryGetValue(TKey key, out TValue value);
 
         public extern static implicit operator Dictionary(Dictionary<TKey, TValue> dictionary);
 
---
+++ src/DSharp.Mscorlib/NonStandard/Collections/Generic/Dictionary.cs
@@ -0,0 +1,22 @@
+﻿using NonStandard;
+
+namespace System.Collections.Generic
+{
+    public sealed partial class Dictionary<TKey, TValue>
+    {
+        [Obsolete("This is only for use by the c# compiler, and cannot be used for generating script.", /* error */ true)]
+        public extern bool TryGetValue(TKey key, out TValue value);
+
+        [Obsolete(ObsoleteConsts.MESSAGE_ON_OBSOLETE, ObsoleteConsts.ERROR_ON_OBSOLETE)]
+        public extern static Dictionary<TKey, TValue> GetDictionary(object o);
+
+        [Obsolete(ObsoleteConsts.MESSAGE_ON_OBSOLETE, ObsoleteConsts.ERROR_ON_OBSOLETE)]
+        public extern IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator();
+
+        [Obsolete(ObsoleteConsts.MESSAGE_ON_OBSOLETE, ObsoleteConsts.ERROR_ON_OBSOLETE)]
+        extern IEnumerator IEnumerable.GetEnumerator();
+
+        [Obsolete(ObsoleteConsts.MESSAGE_ON_OBSOLETE, ObsoleteConsts.ERROR_ON_OBSOLETE)]
+        public extern int Count { get; }
+    }
+}
